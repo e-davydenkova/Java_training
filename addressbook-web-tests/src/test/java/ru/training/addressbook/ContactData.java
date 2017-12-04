e68c9b405cd29873d3ac5819b91f6f8567afae9d
Ekaterina Davydenkova <32132004+e-davydenkova@users.noreply.github.com>
2017-12-04 15:09:17 +0300

    Added Task#4: added CreateNewContact

---
+++ addressbook-web-tests/src/test/java/ru/training/addressbook/ContactData.java
@@ -0,0 +1,73 @@
+package ru.training.addressbook;
+
+public class ContactData {
+    private final String firstName;
+    private final String middleName;
+    private final String lastName;
+    private final String nickname;
+    private final String title;
+    private final String company;
+    private final String address;
+    private final String homePhone;
+    private final String mobilePhone;
+    private final String workPhone;
+    private final String fax;
+
+    public ContactData(String firstName, String middleName, String lastName, String nickname, String title, String company, String address, String home, String homePhone, String mobilePhone, String workPhone, String fax) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.nickname = nickname;
+        this.title = title;
+        this.company = company;
+        this.address = address;
+        this.homePhone = homePhone;
+        this.mobilePhone = mobilePhone;
+        this.workPhone = workPhone;
+        this.fax = fax;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getHomePhone() {
+        return homePhone;
+    }
+
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+}
