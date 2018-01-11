205309b51619e34f9c0a23523ceafae9786eb9ff
Ekaterina Davydenkova <32132004+e-davydenkova@users.noreply.github.com>
2018-01-11 11:08:49 +0300

    Added cache for results and preliminary checks

--- addressbook-web-tests/src/test/java/ru/training/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/training/addressbook/tests/GroupCreationTests.java
@@ -17,9 +17,9 @@ public class GroupCreationTests extends TestBase {
 
         GroupData group = new GroupData().withName("test2");
         app.group().create(group);
+        assertThat(app.group().count(), equalTo(before.size() + 1));
 
         Groups after = app.group().all();
-        assertThat(after.size(), equalTo(before.size() + 1));
         assertThat(after, equalTo(
                 before.withAdded(group.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
     }
