d2a5eda270a40707b7385aef110a528f267967f2
Ekaterina Davydenkova <32132004+e-davydenkova@users.noreply.github.com>
2018-01-18 13:29:19 +0300

    Added data comparison from DB not UI for GroupModificationTests

--- addressbook-web-tests/src/test/java/ru/training/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/training/addressbook/tests/HbConnectionTest.java
@@ -10,6 +10,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.training.addressbook.model.ContactData;
 import ru.training.addressbook.model.GroupData;
+import ru.training.addressbook.model.Groups;
 
 import java.util.List;
 
@@ -35,12 +36,24 @@ public class HbConnectionTest {
     }
 
     @Test
-    public void testHbConnection() {
+    public void testHbConnectionContacts() {
         Session session = sessionFactory.openSession();
         session.beginTransaction();
         List<ContactData> result = session.createQuery( "from ContactData where deprecated = '0000-00-00'" ).list();
         for ( ContactData contact : result ) {
             System.out.println(contact);
+        }
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    @Test
+    public void testHbConnectionGroups() {
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<GroupData> result = session.createQuery( "from GroupData" ).list();
+        for ( GroupData group : result ) {
+            System.out.println(group);
         }
         session.getTransaction().commit();
         session.close();
